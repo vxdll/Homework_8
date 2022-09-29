c36a7722f4e2fb380aeca11b956f6405e3750188
Григорий Васякин <vxdll.39@gmail.com>
2022-09-29 15:31:05 +0200

    Task62

---
+++ Task62/Program.cs
@@ -0,0 +1,41 @@
+﻿//Задача 62. Заполните спирально массив 4 на 4. 
+int n = 4;
+int[,] matrix = new int[n, n];
+int temp = 1;
+int i = 0;
+int j = 0;
+
+while (temp <= matrix.GetLength(0) * matrix.GetLength(1))
+{
+    matrix[i, j] = temp;
+    temp++;
+    if (i <= j + 1 && i + j < matrix.GetLength(1) - 1)
+        j++;
+    else if (i < j && i + j >= matrix.GetLength(0) - 1)
+        i++;
+    else if (i >= j && i + j > matrix.GetLength(1) - 1)
+        j--;
+    else
+        i--;
+}
+
+WriteArray(matrix);
+
+void WriteArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            if (array[i, j] / 10 <= 0)
+            {
+                Console.Write($" {array[i, j]} ");
+            }
+            else
+            {
+                Console.Write($"{array[i, j]} ");
+            }
+        }
+         Console.WriteLine();
+    }
+}
